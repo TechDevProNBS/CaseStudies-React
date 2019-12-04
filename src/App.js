f56a94073a79e2cc726169f42950af3f24aff530
imo58@hotmail.co.uk <imo58@hotmail.co.uk>
2019-12-04 11:07:32 +0000

    added inital search/fiter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import ShowRecords from './components/showrecords.component.js';
 import SingleRecord from './components/singlerecordview.component.js';
 import Header from './components/header.component.js';
+import SearchBar from './components/searchbar.component.js';
 
 
 export default class Nationwide extends React.Component{
@@ -14,6 +15,7 @@ export default class Nationwide extends React.Component{
             <div>
                 <Route exact path="/" component={ShowRecords}/>
                 <Route exact path="/full/:var?" component={SingleRecord}/>
+                <Route exact path="/search" component={SearchBar}/>
             </div>
         </Router>
     </div>
