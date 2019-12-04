e607c02c4f83f7b39eeaf8097551bb60205bdeb3
Michael <m.g.k@outlook.com>
2019-12-04 09:59:10 +0000

    initial project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import ShowRecords from './components/showrecords.component.js';
+import SingleRecord from './components/singlerecordview.component.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class Nationwide extends React.Component{
+  render(){
+    return (
+      <div className="bg-light">
+        <Router>
+            <div>
+                <Route exact path="/" component={ShowRecords}/>
+                <Route exact path="/full/:var?" component={SingleRecord}/>
+            </div>
+        </Router>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+
---
+++ src/components/singlerecordview.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { Redirect } from 'react-router-dom';
+
+
+
+export default class Nationwide extends React.Component {
+    render() {
+
+        return (
+            <div>
+                placeholder
+            </div>
+        );
+    }
+}
