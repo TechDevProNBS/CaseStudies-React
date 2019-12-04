fb7f28aed89c1626463823cec19eecfac6434842
imo58@hotmail.co.uk <imo58@hotmail.co.uk>
2019-12-04 10:58:06 +0000

    initial header created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import ShowRecords from './components/showrecords.component.js';
 import SingleRecord from './components/singlerecordview.component.js';
+import Header from './components/header.component.js';
+
 
 export default class Nationwide extends React.Component{
   render(){
     return (
       <div className="bg-light">
+        <Header/>
         <Router>
             <div>
                 <Route exact path="/" component={ShowRecords}/>
