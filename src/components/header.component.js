df04f2ce5d4f41eaa288fa7e8a4c618835f5d26b
Michael <m.g.k@outlook.com>
2019-12-06 09:34:18 +0000

    started integration of features and fixed merge conflicts

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,10 +1,10 @@
 import React from 'react';
 //import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+//import Button from 'react-bootstrap/Button';
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import './css/header.css'
 import logoimage from '../images/logo.png'
-import {Navbar, Nav, NavItem } from 'react-bootstrap'
+//import {Navbar, Nav, NavItem } from 'react-bootstrap'
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 export default class Mine extends React.Component {
@@ -15,18 +15,21 @@ export default class Mine extends React.Component {
                 {/* <header class="header" id="header"> */}
                 {/* <div class="container"> */}
                 {/* <div class="row"> */}
-                <div class="col-12">
-                    <a className="logo d-none d-lg-inline-block" href="/"><img src={logoimage} class="img-fluid" alt="Nationwide Building Society" width="130" height="52"></img>
-                        <a class="logo d-lg-none" href="/"><img src="/images/logo-mobile.png" class="img-fluid" alt="Nationwide Building Society" width="45" height="45"></img>
+                <div className="col-12">
+                    <a className="logo d-none d-lg-inline-block" href="/"><img src={logoimage} className="img-fluid" alt="Nationwide Building Society" width="130" height="52"></img>
+                        <a className="logo d-lg-none" href="/"><img src="/images/logo-mobile.png" className="img-fluid" alt="Nationwide Building Society" width="45" height="45"></img>
                         </a>
                     </a>
                     <div style={{ float: "right" }}>
-                        <a class="header-btn header-home" href="/home/" aria-label="Home" id="js-home">
+                        <a className="header-btn header-home" href="/" aria-label="Home" id="js-home">
                             <span>Home</span>
                         </a>
-                        <a class="header-btn" href="/search/">
+                        <a className="header-btn header-home" href="/showtable" aria-label="Home" id="js-home">
+                            <span>TDP Profiles</span>
+                        </a>
+                        <a className="header-btn" href="/login">
 
-                            <span>Search</span>
+                            <span>Login</span>
                         </a>
 
                     </div >
